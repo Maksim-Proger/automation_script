016e3dbf0f85b25e31af8ff420cc6e3c6b8a9c30
Maksim <mpozdnyakov@servicepipe.ru>
2025-11-13 13:26:09 +0300

    11

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 # Папка и файл для наблюдения
-WATCH_DIR = "путь к файлу"
-WATCH_FILENAME = "название файла"
+WATCH_DIR = "etc/myapp/"
+WATCH_FILENAME = "config.yaml"
 
 # Дебаунс
 DEBOUNCE_SECONDS = 1.5
@@ -9,17 +9,17 @@ DEBOUNCE_SECONDS = 1.5
 # Настройки вспомогательных серверов
 SERVERS = [
     {
-        "host": "192.168.0.101",
+        "host": "192.168.56.102",
         "port": 22,
-        "username": "имя пользователя на вспомогательном сервере",
-        "key_filename": "путь к SSH-ключу",
-        "remote_path": "адрес файла на вспомогательном сервере"
+        "username": "vboxuser",
+        "key_filename": "/home/vboxuser/.ssh/vm2",
+        "remote_path": "etc/myapp/"
     },
-    {
-        "host": "192.168.0.102",
-        "port": 22,
-        "username": "имя пользователя на вспомогательном сервере",
-        "key_filename": "путь к SSH-ключу",
-        "remote_path": "адрес файла на вспомогательном сервере"
-    }
+    # {
+    #     "host": "192.168.0.102",
+    #     "port": 22,
+    #     "username": "имя пользователя на вспомогательном сервере",
+    #     "key_filename": "путь к SSH-ключу",
+    #     "remote_path": "адрес файла на вспомогательном сервере"
+    # }
 ]
